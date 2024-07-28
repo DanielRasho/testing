9186417819353b1043fc248af1e54ffd424e5561
DanielRasho <ray22933@uvg.edu.gt>
2024-07-27 22:42:08 -0600

    fix: test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-// const setupSwagger = require('./docs/swaggerConfig')
+const setupSwagger = require('./docs/swaggerConfig')
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
